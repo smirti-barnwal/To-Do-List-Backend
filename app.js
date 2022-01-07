fe7385f28cdff20fa5db812b2cc3e82dd064f4dd
smirti-barnwal <smirtibarnwal4@gmail.com>
2022-01-07 17:24:17 +0530

    Complete To-do List

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+// console.log(date());
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+
+  let day = date.getDate();
+  // let today = new Date();
+  // let options = {
+  //   weekday: "long",
+  //   day: "numeric",
+  //   month: "long"
+  // };
+  //
+  // let day = today.toLocaleDateString("en-US",options);
+
+  res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req,res){
+
+  // console.log(req.body);
+  let item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+  console.log(item);
+});
+
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+});
